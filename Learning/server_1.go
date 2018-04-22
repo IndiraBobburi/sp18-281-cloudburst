598da59ca77293ade11dc83510c3fcfa27a0f1cc
Kalikalyan Dash <kkdgrece@gmail.com>
2018-04-22 10:33:28 -0700

    go api

--- Learning/server_1.go
+++ Learning/server_1.go
@@ -39,12 +39,6 @@ func initRoutes(mx *mux.Router, formatter *render.Render) {
 	mx.HandleFunc("/orders", postOrderHandler(formatter)).Methods("POST")
 	mx.HandleFunc("/order/{id}", updateOrderHandler(formatter)).Methods("PUT")
 	mx.HandleFunc("/order/{id}", deleteOrderHandler(formatter)).Methods("DELETE")
-
-	//mx.HandleFunc("/gumball", gumballUpdateHandler(formatter)).Methods("PUT")
-	//mx.HandleFunc("/order", gumballNewOrderHandler(formatter)).Methods("POST")
-	//mx.HandleFunc("/order/{id}", gumballOrderStatusHandler(formatter)).Methods("GET")
-	//mx.HandleFunc("/order", gumballOrderStatusHandler(formatter)).Methods("GET")
-	//mx.HandleFunc("/orders", gumballProcessOrdersHandler(formatter)).Methods("POST")
 }
 
 // API Ping Handler
@@ -56,7 +50,7 @@ func pingHandler(formatter *render.Render) http.HandlerFunc {
 }
 
 
-// API Gumball Machine Handler
+// API Machine Handler
 func getOrdersHandler(formatter *render.Render) http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
 		session, err := mgo.Dial(mongodb_server)
@@ -68,6 +62,54 @@ func getOrdersHandler(formatter *render.Render) http.HandlerFunc {
 		c := session.DB(mongodb_database).C(mongodb_collection)
 		var result[] bson.M
 		err = c.Find(bson.M{}).All(&result)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("Orders:", result )
+		formatter.JSON(w, http.StatusOK, result)
+	}
+}
+
+func postOrderHandler(formatter *render.Render) http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+		fmt.Println("Order Post:" )
+		uuid := uuid.NewV4()
+		fmt.Println(uuid.String())
+		fmt.Println(req.Body)
+
+		var order Order
+		_ = json.NewDecoder(req.Body).Decode(&order)
+		order.OrderId = uuid.String()
+		session, err := mgo.Dial(mongodb_server)
+		if err != nil {
+			panic(err)
+		}
+		defer session.Close()
+		session.SetMode(mgo.Monotonic, true)
+		c := session.DB(mongodb_database).C(mongodb_collection)
+		err = c.Insert(order)
+		if err != nil {
+			log.Fatal(err)
+		}
+		formatter.JSON(w, http.StatusOK, order)
+	}
+}
+
+func getOrderHandler(formatter *render.Render) http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+		session, err := mgo.Dial(mongodb_server)
+		params := mux.Vars(req)
+		fmt.Println("params", params )
+
+		if err != nil {
+			panic(err)
+		}
+		defer session.Close()
+
+		session.SetMode(mgo.Monotonic, true)
+		c := session.DB(mongodb_database).C(mongodb_collection)
+		var result bson.M
+		err = c.Find(bson.M{"orderid": params["id"]}).One(&result)
 		if err != nil {
 			log.Fatal(err)
 		}
