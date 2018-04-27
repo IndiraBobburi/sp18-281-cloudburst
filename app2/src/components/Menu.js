f3498c8ff42aa8c6594e5327e2226da9ad7fb981
manasacsy <32827245+manasacsy@users.noreply.github.com>
2018-04-27 15:45:23 -0700

    signup - ui code

--- app2/src/components/Menu.js
+++ app2/src/components/Menu.js
@@ -5,8 +5,27 @@ import React, { Component } from 'react';
 class Menu extends Component {
   render() {
     return (  
-        <div>
-            <span>Menu</span>
+        <div className="row">
+            <div className="col-md-2">
+            </div>
+            <div className="col-md-8">
+            <div className="login-block">
+
+                <span className="login-heading-style">Find Locations Near You</span>
+                <form>
+                    <div>
+                        <label className="login-heading-style" for="zip">Enter Zip Code or City and State:</label>
+                        <input className="login-textbox-style" classname="login-textbox-style" id="zip" maxlength="64" name="zip" type="text" value=""></input>
+                    </div>
+
+                    <button className="login-button" id="btnLogin" type="submit">GO</button>
+
+
+                </form>
+                </div>
+            </div>
+            <div className="col-md-2">
+            </div>
         </div>
     );
   }
--- app2/src/components/Signup.js
+++ app2/src/components/Signup.js
@@ -5,8 +5,49 @@ import React, { Component } from 'react';
 class Signup extends Component {
   render() {
     return (  
-        <div>
-            <span>Signup</span>
+        <div className="row">
+            <div className="col-md-4"></div>
+            <div className="col-md-4 login-block">
+            <form novalidate="novalidate">
+                <div className="padding-bottom-20">
+                <label className="login-heading-style" for="FirstName">First Name</label>
+                    <input className="login-textbox-style" id="FirstName" name="FirstName" type="text" value=""></input>
+            </div>
+                <div className="padding-bottom-20">
+                    <label className="login-heading-style"for="LastName">Last Name</label>
+                    <input className="login-textbox-style" id="LastName" name="LastName" type="text" value=""></input>
+                </div>
+                <div className="padding-bottom-20">
+                    <label className="login-heading-style" for="EmailAddress">Email Address</label>
+                    <input className="login-textbox-style" id="EmailAddress" name="EmailAddress" type="text" value=""></input>
+                </div>
+
+                <div className="padding-bottom-20">
+                    <label className="login-heading-style" for="Password">Password (min 8 characters)</label>
+                    <input className="login-textbox-style" id="Password" name="Password" type="password"></input>
+                </div>
+                <div className="padding-bottom-20">
+                    <label className="login-heading-style" for="ConfirmPassword">Confirm Password</label>
+                    <input className="login-textbox-style" id="ConfirmPassword" name="ConfirmPassword" type="password"></input>
+                </div>
+                    <div className="padding-bottom-20">
+                        <input id="AcceptedTerms" name="AcceptedTerms" type="checkbox" value="true"></input>
+                        <label for="AcceptedTerms">I have read and accepted the
+                            <a href="/help/useragreement" id="ViewTerms" target="_blank"> terms and conditions</a>
+                        </label>
+
+                </div>
+                    <div>
+                        <input id="OptIn" name="OptIn" type="checkbox" value="true"></input>
+                        <label for="OptIn">Send me emails about future deals and features</label>
+                </div>
+                    <button className="login-button" type="submit" id="btnNext">Create Account</button>
+                    <p>
+                        The Counter may send email communications about your account activity and other updates from time to time. These email communications may be disabled on your My Account page. We will never give your contact information to third parties.
+                    </p>
+            </form>
+        </div>
+            <div className="col-md-4"></div>
         </div>
     );
   }
