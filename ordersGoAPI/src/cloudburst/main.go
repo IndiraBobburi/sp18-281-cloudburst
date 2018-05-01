a4c8b220e8987b790f7ddae5b8c8ac306917de39
Indira Priyadarshini Bobburi <31742324+IndiraBobburi@users.noreply.github.com>
2018-05-01 16:09:19 -0700

    Code refactoring for cluster connection

--- ordersGoAPI/src/cloudburst/main.go
+++ ordersGoAPI/src/cloudburst/main.go
@@ -11,54 +11,19 @@ import (
 )
 
 var debug = true
-/*var server1 = "http://localhost:9000"
-var server2 = "http://localhost:9001"
-var server3 = "http://localhost:9002"*/
 
+//connect to tcp ports for cluster
 var s1 = "localhost:8002"
 var s2 = "localhost:8003"
 var s3 = "localhost:8004"
 
 var cluster *riak.Cluster
-//var c *riak.Client
-
-/*type Client struct {
-	Endpoint string
-	*http.Client
-}
-
-var tr = &http.Transport{
-	MaxIdleConns:       10,
-	IdleConnTimeout:    30 * time.Second,
-	DisableCompression: true,
-}
-
-func NewClient(server string) *Client {
-	return &Client{
-		Endpoint:  	server,
-		Client: 	&http.Client{Transport: tr},
-	}
-}
-
-func (c *Client) Ping() (string, error) {
-	resp, err := c.Get(c.Endpoint + "/ping" )
-	if err != nil {
-		fmt.Println("[RIAK DEBUG] " + err.Error())
-		return "Ping Error!", err
-	}
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	if debug { fmt.Println("[RIAK DEBUG] GET: " + c.Endpoint + "/ping => " + string(body)) }
-	return string(body), nil
-}*/
-
-
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hi there! Welcome to goBurger")
 }
 
-func initCluster(){
+func init(){
 	nodeOpts1 := &riak.NodeOptions{
 		RemoteAddress: s1,
 	}
@@ -105,35 +70,6 @@ func initCluster(){
 	}
 }
 
-func init(){
-	initCluster()
-	/*var err error
-
-	c1 := NewClient(server1)
-	msg, err := c1.Ping( )
-	if err != nil {
-		log.Fatal(err)
-	} else {
-		log.Println("Riak Ping Server1: ", msg)
-	}
-
-	c2 := NewClient(server2)
-	msg, err = c2.Ping( )
-	if err != nil {
-		log.Fatal(err)
-	} else {
-		log.Println("Riak Ping Server2: ", msg)
-	}
-
-	c3 := NewClient(server3)
-	msg, err = c3.Ping( )
-	if err != nil {
-		log.Fatal(err)
-	} else {
-		log.Println("Riak Ping Server3: ", msg)
-	}*/
-}
-
 func main() {
 	http.HandleFunc("/hi", handler)
 	http.HandleFunc("/getRestaurants", getRestaurants)
@@ -141,23 +77,13 @@ func main() {
 	http.HandleFunc("/addToCart", addToCart)
 	http.HandleFunc("/viewCart", viewCart)
 	http.HandleFunc("/order", order)
-	//http.HandleFunc("/try", try)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 	defer func() {
 		if err := cluster.Stop(); err != nil {
-			fmt.Println(err.Error())
+			log.Println(err.Error())
 		}
 	}()
 }
-
-/*func try(w http.ResponseWriter, r *http.Request) {
-	err := insertingObjects()
-	if err == nil {
-		w.WriteHeader(http.StatusOK)
-	}else {
-		log.Println(err)
-	}
-}*/
 
 func insertingObjects(bucket string, key string, body []byte) error {
 	obj := &riak.Object{
@@ -227,19 +153,9 @@ func getRestaurants(w http.ResponseWriter, r *http.Request) {
 		pincode := r.URL.Query().Get("pincode")
 
 		if pincode != "" {
-			/*c := NewClient(server1)
-			resp, err := c.Get(c.Endpoint + "/buckets/restaurants/keys/"+pincode )
-			if err != nil {
-				fmt.Println("[RIAK DEBUG] " + err.Error())
-			}
-			defer resp.Body.Close()
-
-			body, err := ioutil.ReadAll(resp.Body)
-			w.Write(body)*/
-
 			resp, err := queryingObjects("restaurants", pincode)
 			if err != nil {
-				fmt.Println("[RIAK DEBUG] " + err.Error())
+				log.Println("[RIAK DEBUG] " + err.Error())
 			}
 
 			w.Write(resp.Values[0].Value)
@@ -249,20 +165,9 @@ func getRestaurants(w http.ResponseWriter, r *http.Request) {
 
 func getMenu(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "GET" {
-		/*c := NewClient(server1)
-		resp, err := c.Get(c.Endpoint + "/buckets/restaurants/keys/menu" )
-
-		if err != nil {
-			fmt.Println("[RIAK DEBUG] " + err.Error())
-		}
-		defer resp.Body.Close()
-
-		body, err := ioutil.ReadAll(resp.Body)
-		w.Header().Set("content-type", "application/json")
-		w.Write(body)*/
 		resp, err := queryingObjects("restaurants", "menu")
 		if err != nil {
-			fmt.Println("[RIAK DEBUG] " + err.Error())
+			log.Println("[RIAK DEBUG] " + err.Error())
 		}
 
 		w.Write(resp.Values[0].Value)
@@ -313,16 +218,6 @@ func addToCart(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
-		//val := bytes.NewBuffer(output)
-
-		/*c := NewClient(server1)
-		resp, err := c.Post(c.Endpoint + "/buckets/cart/keys/"+cart.Id+"?returnbody=true",
-			"application/json", val )
-
-		if err != nil {
-			fmt.Println("[RIAK DEBUG] " + err.Error())
-		}
-		defer resp.Body.Close()*/
 		if insertingObjects("cart",cart.Id, output) == nil {
 			w.WriteHeader(http.StatusInternalServerError)
 		} else {
@@ -341,20 +236,9 @@ func viewCart(w http.ResponseWriter, r *http.Request) {
 
 		if debug { fmt.Println("cart id is :", cartid) }
 
-		/*c := NewClient(server1)
-		resp, err := c.Get(c.Endpoint + "/buckets/cart/keys/"+ cartid )
-
-		if err != nil {
-			fmt.Println("[RIAK DEBUG] " + err.Error())
-		}
-		defer resp.Body.Close()
-
-		body, err := ioutil.ReadAll(resp.Body)
-		w.Write(body)*/
-
 		resp, err := queryingObjects("cart", cartid)
 		if err != nil {
-			fmt.Println("[RIAK DEBUG] " + err.Error())
+			log.Println("[RIAK DEBUG] " + err.Error())
 		}
 
 		w.Write(resp.Values[0].Value)
@@ -363,7 +247,6 @@ func viewCart(w http.ResponseWriter, r *http.Request) {
 
 func order(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "POST" {
-		//c := NewClient(server1)
 		uuid, _ := uuid.NewV4()
 		value := "Order Placed"
 
@@ -373,57 +256,44 @@ func order(w http.ResponseWriter, r *http.Request) {
 			value +
 			"\"}"
 
-		/*resp, err := c.Post(c.Endpoint + "/buckets/orders/keys/"+ uuid.String() +"?returnbody=true",
-			"application/json", strings.NewReader(reqbody) )
-		if err != nil {
-			fmt.Println("[RIAK DEBUG] " + err.Error())
-		}
-		defer resp.Body.Close()
-
-		body, err := ioutil.ReadAll(resp.Body)*/
-
 		if insertingObjects("orders", uuid.String(), []byte(reqbody)) == nil {
 			w.WriteHeader(http.StatusInternalServerError)
 			w.Write([]byte(reqbody))
 		} else {
 			w.WriteHeader(http.StatusOK)
 		}
-	}
-	if r.Method == "PUT" {
+	} else if r.Method == "PUT" {
 		var orderid string
-		orderid = r.URL.Query().Get("orderId")
-		if orderid != "" {
+		value := "Order Processed"
+		orderid = r.URL.Query().Get("orderid")
 
+		log.Println(orderid)
+
+		if orderid != "" {
 			reqbody := "{\"Id\": \"" +
 				orderid +
 				"\",\"OrderStatus\": \"" +
-				"Order processed" +
+				value +
 				"\"}"
 			resp, err := updateObjects("orders", orderid, []byte(reqbody))
 			if err != nil {
-				fmt.Println("[RIAK DEBUG] " + err.Error())
+				log.Println("[RIAK DEBUG] " + err.Error())
 			}
 
 			w.Write(resp.Values[0].Value)
 		}
-	}
-	if r.Method == "GET" {
+	} else if r.Method == "GET" {
 		var orderid string
-		orderid = r.URL.Query().Get("orderId")
+		orderid = r.URL.Query().Get("orderid")
+
+		log.Println("in get")
+		fmt.Println("in get")
+		log.Println(orderid)
+
 		if orderid != "" {
-			/*c := NewClient(server1)
-			resp, err := c.Get(c.Endpoint + "/buckets/cart/keys/"+ orderid )
-			if err != nil {
-				fmt.Println("[RIAK DEBUG] " + err.Error())
-			}
-			defer resp.Body.Close()
-
-			body, err := ioutil.ReadAll(resp.Body)
-			w.Write(body)*/
-
 			resp, err := queryingObjects("orders", orderid)
 			if err != nil {
-				fmt.Println("[RIAK DEBUG] " + err.Error())
+				log.Println("[RIAK DEBUG] " + err.Error())
 			}
 
 			w.Write(resp.Values[0].Value)
